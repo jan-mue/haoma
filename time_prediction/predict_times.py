68b0ed38e49cc9d09bcc5a9ea545093b7caf76a4
Jan Müller <jan.rv@t-online.de>
2019-09-07 21:04:03 +0200

    adapted model & save model

--- time_prediction/predict_times.py
+++ time_prediction/predict_times.py
@@ -1,34 +1,25 @@
 import tensorflow as tf
 
-from tensorflow.keras.layers import Dense, Flatten, Conv2D
-from tensorflow.keras import Model
+from tensorflow.keras import layers
 
 from patient_data import load_data
 
 features, waiting_times, procedure_times, punctuality_times = load_data("data.csv")
 
-train_ds = tf.data.Dataset.from_tensor_slices((features, procedure_times)).shuffle(1000).batch(128)
+ds = tf.data.Dataset.from_tensor_slices((features, waiting_times))
+train_ds = ds.take(80_000).shuffle(1000).batch(16)
+test_ds = ds.skip(80_000).batch(16)
 
-
-class WaitingTimeModel(Model):
-    def __init__(self):
-        super(WaitingTimeModel, self).__init__()
-        self.d1 = Dense(64, activation="relu")
-        self.d2 = Dense(64, activation="relu")
-        self.d3 = Dense(1)
-
-    def call(self, x):
-            x = self.d1(x)
-            x = self.d2(x)
-            return self.d3(x)
-
-
-model = WaitingTimeModel()
+model = tf.keras.models.Sequential([
+    layers.Dense(12, activation="relu"),
+    layers.Dense(1, activation="relu")
+  ])
 
 loss_object = tf.keras.losses.MeanSquaredError()
 optimizer = tf.keras.optimizers.Adam()
 
 train_loss = tf.keras.metrics.Mean(name='train_loss')
+test_loss = tf.keras.metrics.Mean(name='test_loss')
 
 
 @tf.function
@@ -42,14 +33,31 @@ def train_step(features, values):
     train_loss(loss)
 
 
-EPOCHS = 5
+@tf.function
+def test_step(images, labels):
+  predictions = model(images)
+  t_loss = loss_object(labels, predictions)
+
+  test_loss(t_loss)
+
+
+EPOCHS = 8
 
 for epoch in range(EPOCHS):
     for features, values in train_ds:
         train_step(features, tf.reshape(values, [-1, 1]))
 
-    template = 'Epoch {}, Loss: {}'
-    print(template.format(epoch + 1, train_loss.result()))
+    for test_images, test_labels in test_ds:
+        test_step(test_images, test_labels)
+
+    template = 'Epoch {}, Loss: {}, Test Loss: {}'
+    print(template.format(epoch + 1,
+                          train_loss.result(),
+                          test_loss.result()))
 
     # Reset the metrics for the next epoch
     train_loss.reset_states()
+    test_loss.reset_states()
+
+model.summary()
+model.save('ge_waiting_time_model.h5')
